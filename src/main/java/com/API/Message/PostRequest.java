344ae5d2e1c6f2439f9600dfcdf56403aaadd8d2
testpracticeap <testpracticeapi68@gmail.com>
2022-09-30 15:54:20 +0530

    Program

--- src/main/java/com/API/Message/PostRequest.java
+++ src/main/java/com/API/Message/PostRequest.java
@@ -3,6 +3,7 @@ package com.API.Message;
 import java.util.List;
 
 import javax.swing.JList;
+
 public class PostRequest {
 	private String Name;
 	private String Address;
@@ -10,48 +11,44 @@ public class PostRequest {
 	private String Technicaldomain;
 	private String Designation;
 
-	 public String getName() {
-			return Name;
-		      }
-
-		    public void setName(String Name) {
-			this.Name= Name;
-		      }
-		    public String getAddress() {
-		    return Address;
-		          }
-
-		    public void setAddress(String Address) {
-		    this.Address= Address;
-		          }
-		        
-		     public int getEmpID() {
-		        	return EmpID;
-		              }
-
-		     public void setEmpID(int EmpID) {
-		        	this.EmpID= EmpID;
-		              }
-		     public String getTechnicaldomain() {
-		            	return Technicaldomain;
-		                  }
-
-		      public void setTechnicaldomain(String Technicaldomain) {
-		            	this.Technicaldomain= Technicaldomain;
-		                  }
-		      public String getDesignation() {
-		                	return Designation;
-		                      }
-
-		       public void setDesignation(String Designation) {
-		                	this.Designation= Designation;
-		                      }
-
-		
-		
+	public String getName() {
+		return Name;
 	}
-	
-	
-	
-	
 
+	public void setName(String Name) {
+		this.Name = Name;
+	}
+
+	public String getAddress() {
+		return Address;
+	}
+
+	public void setAddress(String Address) {
+		this.Address = Address;
+	}
+
+	public int getEmpID() {
+		return EmpID;
+	}
+
+	public void setEmpID(int EmpID) {
+		this.EmpID = EmpID;
+	}
+
+	public String getTechnicaldomain() {
+		return Technicaldomain;
+	}
+
+	public void setTechnicaldomain(String Technicaldomain) {
+		this.Technicaldomain = Technicaldomain;
+	}
+
+	public String getDesignation() {
+		return Designation;
+	}
+
+	public void setDesignation(String Designation) {
+		this.Designation = Designation;
+	}
+
+}
