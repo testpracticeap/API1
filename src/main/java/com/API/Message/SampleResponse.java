872a1daed5ef2ea38801b37d7b704d0c2f8784ee
testpracticeap <testpracticeapi68@gmail.com>
2022-09-30 15:56:10 +0530

    program

--- src/main/java/com/API/Message/SampleResponse.java
+++ src/main/java/com/API/Message/SampleResponse.java
@@ -9,55 +9,58 @@ public class SampleResponse {
 	private int EmpID;
 	private String Technicaldomain;
 	private String Designation;
+
 	public SampleResponse() {
-		
+
 	}
-	public SampleResponse(String Name,String Address, String Technicaldomain, String Designation,int EmpID )
-	{
-		this.Name =Name;
-		this.Address =Address;
-		this.EmpID =EmpID;
-		this.Technicaldomain =Technicaldomain;
-		this.Designation =Designation;
-		
-		
+
+	public SampleResponse(String Name, String Address, String Technicaldomain, String Designation, int EmpID) {
+		this.Name = Name;
+		this.Address = Address;
+		this.EmpID = EmpID;
+		this.Technicaldomain = Technicaldomain;
+		this.Designation = Designation;
+
 	}
-	
-    public String getName() {
-	return Name;
-      }
 
-    public void setName(String Name) {
-	this.Name= Name;
-      }
-    public String getAddress() {
-    return Address;
-          }
+	public String getName() {
+		return Name;
+	}
 
-    public void setAddress(String Address) {
-    this.Address= Address;
-          }
-        
-     public int getEmpID() {
-        	return EmpID;
-              }
+	public void setName(String Name) {
+		this.Name = Name;
+	}
 
-     public void setEmpID(int EmpID) {
-        	this.EmpID= EmpID;
-              }
-     public String getTechnicaldomain() {
-            	return Technicaldomain;
-                  }
+	public String getAddress() {
+		return Address;
+	}
 
-      public void setTechnicaldomain(String Technicaldomain) {
-            	this.Technicaldomain= Technicaldomain;
-                  }
-      public String getDesignation() {
-                	return Designation;
-                      }
+	public void setAddress(String Address) {
+		this.Address = Address;
+	}
 
-       public void setDesignation(String Designation) {
-                	this.Designation= Designation;
-                      }
+	public int getEmpID() {
+		return EmpID;
+	}
+
+	public void setEmpID(int EmpID) {
+		this.EmpID = EmpID;
+	}
+
+	public String getTechnicaldomain() {
+		return Technicaldomain;
+	}
+
+	public void setTechnicaldomain(String Technicaldomain) {
+		this.Technicaldomain = Technicaldomain;
+	}
+
+	public String getDesignation() {
+		return Designation;
+	}
+
+	public void setDesignation(String Designation) {
+		this.Designation = Designation;
+	}
 
 }
